6a2bdffda69d1e6e758dd72ff4b8f1f9c91eb231
Fabio-Spiller <devfabiospiller@gmail.com>
2025-10-07 20:23:53 -0300

    spring

---
+++ checkinexpress/src/main/java/br/pucpr/checkinexpress/CheckinexpressApplication.java
@@ -0,0 +1,13 @@
+package br.pucpr.checkinexpress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckinexpressApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckinexpressApplication.class, args);
+	}
+
+}
